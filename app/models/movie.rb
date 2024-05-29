a1591bf55e39fd85cf3a9f0385fc72b10e109deb
Jeremy Rome <jeremy.rome93@gmail.com>
2024-05-29 14:31:16 +0200

    Seed finished

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,12 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
+  has_many :events
+  has_many :reviews
+  # include PgSearch::Model
+
+  # # pg_search_scope :search_movies,
+  # # against: [ :title, :description, :genre ],
+  # # using: {
+  # #   tsearch: { prefix: true }
+  # # }
 end
