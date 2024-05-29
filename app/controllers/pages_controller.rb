2e654cc67c2a1a31530cef780d2df0a3ec6c269f
Roberto Piazzolla <sertopico@gmail.com>
2024-05-29 15:15:21 +0200

    add logic for user preferences and customize sign up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,12 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def index
+    @recommended_movies = Movie.where("genre ILIKE ANY (array[?]) OR director ILIKE ANY (array[?]), OR actors ILIKE ANY (array[?])",
+                                      current_user.favorite_genres.split(',').map { |g| "%#{g}%}" },
+                                      current_user.favorite_directors.split(',').map { |d| "%#{d}%}" },
+                                      current_user.favorite_actors.split(',').map { |a| "%#{a}%}" })
+    @movie_buddies = current_user.find_movie_buddies
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # This is made in order to validate the user's preferences
+  validates :favorite_genres, presence: true
+  validates :favorite_directors, presence: true
+  validates :favorite_actors, presence: true
+
+  def find_movie_buddies
+    User.where.not(id: self.id)
+        .where("favorite_genres && ARRAY[?]::varchar[] OR favorite_directors && ARRAY[?]::varchar[] OR favorite_actors && ARRAY[?]::varchar[]",
+               self.favorite_genres.split(','),
+               self.favorite_directors.split(','),
+               self.favorite_actors.split(','))
+  end
 end
