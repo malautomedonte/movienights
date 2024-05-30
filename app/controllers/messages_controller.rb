52cdcb0430b8acb0db1a44685b1afb2ae896bac7
Jeremy Rome <jeremy.rome93@gmail.com>
2024-05-30 17:53:45 +0200

    Chat function added

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,24 @@
+class MessagesController < ApplicationController
+  def create
+    @event = Event.find(params[:event_id])
+    @message = Message.new(message_params)
+    @message.event = @event
+    @message.user = current_user
+    if @message.save
+      EventChannel.broadcast_to(
+        @event,
+        render_to_string(partial: "message", locals: {message: @message})
+      )
+      head :ok
+      # redirect_to event_path(@event)
+    else
+      render "events/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
