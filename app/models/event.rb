a7f42f1e101899c11732944417abef6c3dd450f4
Marco Baass <picture@marcobaass.com>
2024-05-31 17:00:04 +0200

    events-map

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,5 +6,8 @@ class Event < ApplicationRecord
   has_many :event_users, dependent: :destroy
   has_many :messages
 
-  validates :title, :date, :location, presence: true
+  validates :date, :location, presence: true
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
