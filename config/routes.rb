566c45e424126f17a65af2c09f3f47e33d13a4b3
Marco Baass <picture@marcobaass.com>
2024-06-03 17:37:10 +0200

    events-cosmetics

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations'}
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
   root "pages#home"
 
-  # resources :bookmarks, only:[:create, :destroy, :index ]
-  resources :movies, only:[:index]
+  # resources :bookmarks, only: [:create, :destroy, :index]
+  resources :movies, only: :index
   resources :events do
-  resources :messages, only: :create
+    resources :messages, only: :create
   end
+
   resources :event_users, only: %i[create update destroy]
-  resources :friendships, only: [:index, :create]
-  get 'profile/:id', to: 'pages#profile', as: "profile"
+  resources :friendships, only: %i[index create]
+
+  get '/profile/:id', to: 'pages#profile', as: "profile"
   get '/pages/myevents', to: 'pages#myevents'
   get '/friendrequests', to: 'friendships#friend_requests'
   patch '/accept', to: 'friendships#accept'
