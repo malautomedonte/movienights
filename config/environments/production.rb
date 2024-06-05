c470d4b159a2643ee2f0a17c1915547c85a3f117
Marco Baass <picture@marcobaass.com>
2024-06-05 14:14:48 +0200

    redis2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,5 +96,4 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.serve_static_assets = true
-  config.cache_store = :redis_cache_store, { url: ENV.fetch['REDIS_URL'] }
 end
